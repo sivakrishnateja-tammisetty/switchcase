3a681046d498ce3a9ad9a009c9a2117ad1113e8b
sivakrishnateja-tammisetty <sivakrishnateja07@gmail.com>
2023-01-27 18:47:25 +0530

    added

---
+++ operators.c
@@ -0,0 +1,323 @@
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=10,b=5;
+	printf("%d\n",(a&b));//0
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=10,b=5;
+	printf("%d\n",(a|b));//15
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=10,b=5;
+	printf("%d\n",(a^b));//15
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=10,b=6;
+	printf("%d\n",a&b);//2
+	printf("%d\n",a|b);//14
+	printf("%d\n",a^b);//12
+	printf("%d\n",a&b && b+1 || 0);//1
+	return 0;
+}
+*/
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	char  a='A',b='C';
+	printf("%d\n",a&b);//65
+	return 0;
+	}
+	
+*/
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	printf("%d\n",a<<2);//40
+	//varaible*2 pow shift=ans
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+int a=10;
+printf("%d\n",a>>2);//2
+//variable/2powshift=ans
+return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=6;
+	printf("%d\n",~a);//-7
+	// -(a+1)
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=12,b=10;
+	a>b ?printf("a is greater  "):printf("b is greater");
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int age=21;
+	age>=18 ?printf("You  can vote"):printf("You  cannot vote");
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	char optr='-';
+	int a=2,b=3;
+	int result=(optr=='+') ? (a+b) : (a-b);
+	printf("%d\n",result);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int num=13;
+	num%2==0 ?printf("Even number"):printf("Odd number");
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=3,b=12,c=6;
+      int big=	(a>b && a>c) ? a:((b>c) ?b:c);
+      printf("%d\n",big);
+	return 0;
+
+}
+*/
+
+
+
+
+
+
+
+
+/*
+
+#include<stdio.h>
+int main()
+{
+	int x=11,y=22,z=33;
+	printf("%d\n",x);//11
+	printf("%d\n",y);//22
+	printf("%d\n",z);//33
+	++x;++y;++z;
+	printf("%d\n",x);//12
+	printf("%d\n",y);//23
+	printf("%d\n",z);//34
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+#include<stdio.h>
+int main()
+{
+int x=11,y=22,z=33;
+printf("%d\n",x);//11
+printf("%d\n",y);//22
+printf("%d\n",z);//33
+printf("%d\n",x++);//11
+printf("%d\n",y++);//22
+printf("%d\n",z++);//33
+return 0;
+}
+*/
+
+
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a=3,b=4;
+	printf("3 & 4 =%d\n",a&b);//0
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+/*
+
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	int b=++(++a);
+	printf("%d\n",b);
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
